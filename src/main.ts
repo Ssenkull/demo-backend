fae6c51b84ab30f6595a2077792f4cf21c37d2eb
Богдан Збаражский <tension42@MacBook-Pro-Bogdan.local>
2026-03-05 21:10:07 +0200

    fix: allow all origins for CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:8080'],
+    origin: '*',
     methods: ['GET', 'POST', 'DELETE'],
   });
 
